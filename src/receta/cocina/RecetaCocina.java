b959592487dfa247d8ddee6286ca08cae914fb2d
samue <samue@DESKTOP-8PFTI10>
2020-04-30 20:05:21 +0200

    Interfaces y primeras recetas realizadas. Además de añadir los botones de volver

---
+++ src/receta/cocina/RecetaCocina.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package receta.cocina;
+
+/**
+ *
+ * @author samue
+ */
+public class RecetaCocina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interfaz obx= new Interfaz();
+        obx.setVisible(true);
+    }
+    
+}
